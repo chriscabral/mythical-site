b31ab6c566a7deaed5fe50ffb560e90916c96980
Chris Cabral <chris.cabral@newbrandanalytics.com>
2013-11-13 10:54:07 -0500

    trying to change the css

--- settings.py
+++ settings.py
@@ -112,7 +112,7 @@ LANGUAGE_CODE = "en"
 # A boolean that turns on/off debug mode. When set to ``True``, stack traces
 # are displayed for error pages. Should always be set to ``False`` in
 # production. Best set to ``True`` in local_settings.py
-DEBUG = True
+DEBUG = False
 
 # Whether a user's session cookie expires when the Web browser is closed.
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
@@ -227,7 +227,7 @@ TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 
 INSTALLED_APPS = (
 #     "mythical_theme",
-#     "mezzanine_default",
+    "mezzanine_default",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
