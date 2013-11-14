e296a98256bca8ef36d88859d2c576b1f3cb097a
Chris Cabral <chris.cabral@newbrandanalytics.com>
2013-11-13 22:10:32 -0500

    complete delete models

--- mythial_pm/models.py
+++ mythial_pm/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import User
 from mezzanine.pages.models import Page
-
-# class Project(Page):
-#     name = models.CharField(max_length=200)
-#     owner = models.ForeignKey(User, editable=False)
-#     has_been_created = models.BooleanField(default=False)
-#     is_on = models.BooleanField(default=False)
-#     public_dns = models.CharField(max_length=500)
-#     private_key = models.CharField(max_length=1000)
-    
